17e00d4ac9aef61d90ee8b3ae80dbfbc289d852e
Joseph Knoebel <knoebelja@gmail.com>
2017-09-13 17:36:04 -0400

    working on test specs

--- server/db/models/order.spec.js
+++ server/db/models/order.spec.js
@@ -23,13 +23,53 @@ describe('Order model', () => {
   })
 
   describe('validations', () => {
-    it('requires quantity to be greater than 0', {
+    const order1 = Order.build({quantity: 0,price: 2})
+    const order2 = Order.build({quantity: 9, price: -5})
+    const order3 = Order.build({quantity: 213813, price: 2939})
 
+    describe ('requires quantity to be greater than 0', () => {
+      it('fails when quantity is less than 1', () => {
+        return order1.validate()
+          .then(() => {
+            throw Error(`can't set quantity to less than 1`)
+          },
+          (err) => {
+            expect(err.message).to.contain('Validation min on quantity failed')
+          })
+        })
+      it('passes when quantity is greater than 0', () => {
+        return order3.validate()
+      })
     })
 
-    it('requires price to be greater than 0', {
-
+    describe('requires price to be greater than 0', () => {
+      it('fails when price is less than 1', () => {
+        return order2.validate()
+          .then(() => {
+            throw Error(`can't set price to less than 1`)
+          },
+          (err) => {
+            expect(err.message).to.contain('Validation min on price failed')
+          })
+        })
+      it('passes when priceis greater than 0', () => {
+        return order3.validate()
+      })
     })
+
+  })
+
+  describe('Getter Methods', () => {
+
+    const order1 = Order.build({quantity: 5, price: 999})
+
+    describe('total price', () => {
+
+      it('returns the price and total product divided by 100', () => {
+        expect(order1.totalPrice)
+      })
+    })
+
   })
 
 })
