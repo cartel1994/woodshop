38abb3f6fef2e1d0f3222afe5fa362317cd9ed69
Joseph Knoebel <knoebelja@gmail.com>
2017-09-14 11:48:12 -0400

    product ready for request

---
+++ server/db/models/product.js
@@ -0,0 +1,31 @@
+const Sequelize = require('sequelize')
+const db = require('../db')
+
+const Product = db.define('product', {
+  name: {
+    type: Sequelize.STRING,
+    allowNull: false,
+    unique: true,
+    validate: {notEmpty: true}
+  },
+  details: {
+    type: Sequelize.TEXT
+  },
+  available: {
+    type: Sequelize.BOOLEAN,
+    default: false
+  },
+  price: {
+    type: Sequelize.INTEGER,
+    allowNull: false,
+    validate: {min: 1}
+  }
+}, {
+  getterMethods: {
+    price: function() {
+      return this.getDataValue('price') / 100
+    }
+  }
+})
+
+module.exports = Product
