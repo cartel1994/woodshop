2c55eecfc29e2b5ac6cd694162db2ceb32f7def6
Daniel Onggunhao <danielo@cs.stanford.edu>
2017-09-16 01:32:19 +0700

    Add seed for categories

--- seed/seed.js
+++ seed/seed.js
@@ -7,10 +7,23 @@ const Order = require('../server/db/models/order')
 const Category = require('../server/db/models/category')
 const Review = require('../server/db/models/review')
 
+// Mockaroo seed data
 const products = require('./productData')
+
+// Non-mockaroo seed data
+const categories = [
+  { name: 'Furniture' },
+  { name: 'Shipbuilding' },
+  { name: 'Fencing' },
+  { name: 'Plywood' },
+  { name: 'Clearance' }
+]
+
+// Assigns ids for the Mockaroo sample data
 
 const seed = () => 
   Promise.all(products.map(product => Product.create(product)))
+  .then(() => Promise.all(categories.map(category => Category.create(category))))
 
 const main = () => {
   console.log('Syncing db...');
