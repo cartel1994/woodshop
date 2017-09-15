8f6622e3c5c129046f6e9fc7083e0acf6c4327ec
Daniel Onggunhao <danielo@cs.stanford.edu>
2017-09-16 02:16:25 +0700

    Add seed data for Purchase, Review, and User

--- seed/seed.js
+++ seed/seed.js
@@ -6,9 +6,13 @@ const Purchase = require('../server/db/models/purchase')
 const Order = require('../server/db/models/order')
 const Category = require('../server/db/models/category')
 const Review = require('../server/db/models/review')
+const User = require('../server/db/models/user')
 
 // Mockaroo seed data
 const products = require('./productData')
+const purchases = require('./purchaseData')
+const users = require('./userData')
+const reviews = require('./reviewData')
 
 // Non-mockaroo seed data
 const categories = [
@@ -20,10 +24,15 @@ const categories = [
 ]
 
 // Assigns ids for the Mockaroo sample data
+// -- Todo: Assign UserIds for each purchase
+// -- Todo: Assign UserIDs and productIDs for each review
+// -- Todo:  
 
 const seed = () => 
   Promise.all(products.map(product => Product.create(product)))
   .then(() => Promise.all(categories.map(category => Category.create(category))))
+  .then(() => Promise.all(purchases.map(purchase => Purchase.create(purchase))))
+  .then(() => Promise.all(users.map(user => User.create(user))))
 
 const main = () => {
   console.log('Syncing db...');
