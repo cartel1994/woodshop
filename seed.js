251ce1dc34d81f97e7959329951575ae5d85f245
Daniel Onggunhao <danielo@cs.stanford.edu>
2017-09-15 20:39:10 +0700

    Added seed file

---
+++ seed.js
@@ -0,0 +1,9 @@
+const db = require('./server/db')
+
+// Models
+const Product = require('./server/db/models/product')
+const Purchase = require('./server/db/models/purchase')
+const Order = require('./server/db/models/order')
+const Category = require('./server/db/models/category')
+const Review = require('./server/db/models/review')
+
