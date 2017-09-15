ad2e99c1fc84faf0195b43858b2865f67392f7d9
Daniel Onggunhao <danielo@cs.stanford.edu>
2017-09-16 01:18:52 +0700

    Create seed for Products

--- seed/seed.js
+++ seed/seed.js
@@ -1,9 +1,33 @@
-const db = require('./server/db')
+const db = require('../server/db')
 
 // Models
-const Product = require('./server/db/models/product')
-const Purchase = require('./server/db/models/purchase')
-const Order = require('./server/db/models/order')
-const Category = require('./server/db/models/category')
-const Review = require('./server/db/models/review')
+const Product = require('../server/db/models/product')
+const Purchase = require('../server/db/models/purchase')
+const Order = require('../server/db/models/order')
+const Category = require('../server/db/models/category')
+const Review = require('../server/db/models/review')
 
+const products = require('./productData')
+
+const seed = () => 
+  Promise.all(products.map(product => Product.create(product)))
+
+const main = () => {
+  console.log('Syncing db...');
+  db.sync({ force: true })
+    .then( () => {
+      console.log('Seeding database');
+      return seed();
+    })
+    .catch(err => {
+      console.log('Error while seeding');
+      console.log(err.stack)
+      console.log(err)
+    })
+    .then(() => {
+      db.close();
+      return null;
+    })
+}
+
+main();
