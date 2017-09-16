3c31ef08cc70301d384c9d85bc395d4471462353
Daniel Onggunhao <danielo@cs.stanford.edu>
2017-09-16 07:53:29 +0700

    Add associations to seed script

--- seed/seed.js
+++ seed/seed.js
@@ -24,9 +24,8 @@ const categories = [
 ]
 
 // Assigns ids for the Mockaroo sample data
-// -- Todo: Assign UserIds for each purchase
-// -- Todo: Assign UserIDs and productIDs for each review
-// -- Todo:  
+// -- Todo: Orders: Create orders that have productId and purchaseId
+// .map(review => review.setProduct( Math.round(Math.random() * (products.length - 1)) + 1 ))
 
 const seed = () => 
   Promise.all(products.map(product => Product.create(product)))
@@ -35,12 +34,31 @@ const seed = () =>
   .then(() => Promise.all(users.map(user => User.create(user))))
   .then(() => Promise.all(reviews.map(review => Review.create(review))))
 
+const makeAssociations = () => {
+
+  return Purchase.findAll()
+    .then(purchases => {
+      return Promise.all(purchases.map(purchase => purchase.setUser( Math.round(Math.random() * (users.length - 1)) + 1 )))
+      })
+    .then(() => Review.findAll())
+      .then(reviews => {
+        return Promise.all(reviews.map(review => review.setUser( Math.round(Math.random() * (users.length - 1)) + 1 )))
+      })
+    .then(() => Review.findAll())
+      .then(reviews => {
+        return Promise.all(reviews.map(review => review.setProduct( Math.round(Math.random() * (products.length - 1)) + 1 )))
+      })
+}
+
 const main = () => {
   console.log('Syncing db...');
   db.sync({ force: true })
     .then( () => {
       console.log('Seeding database');
       return seed();
+    })
+    .then(() => {
+      return makeAssociations();
     })
     .catch(err => {
       console.log('Error while seeding');
