562bda99390d182c2ce8f45107ba127547cda670
Daniel Onggunhao <danielo@cs.stanford.edu>
2017-09-17 19:15:31 +0700

    Create express GET route for cart

--- server/api/cart.js
+++ server/api/cart.js
@@ -2,8 +2,16 @@ const router = require('express').Router();
 
 router.post('/', (req, res, next) => {
   console.log("==============")
+  console.log(req.body)
   req.session.cart = req.body;
   res.status(201)
+})
+
+router.get('/', (req, res, next) => {
+  let cart = req.session.cart
+  console.log(req.body)
+  res.json(cart)
+  
 })
 
 
